1bcf19f9ccf2805d0556da3908ab07e340e9b1f9
Jongsoo Yoon <jongsoojong@gmail.com>
2016-11-27 20:59:21 -0800

    Chat rooms are working

--- client/controller/chatController.js
+++ client/controller/chatController.js
@@ -1,36 +1,121 @@
 angular.module('jongChat')
   .controller('chatController', function($scope, $state, $http, $location, socket, userInfo, getChatData){
     var vm = this;
-    vm.msgs = [];
     var thisDate = new Date;
+
     vm.messagePrompt =  userInfo.currentUsername + " (" + thisDate.toLocaleString() + ")" + ' : '
+    vm.msgs = [];
+    vm.hangoutmsgs = [];
+    vm.hrmsgs = [];
+    vm.mksmsgs = [];
+    vm.mksState = true;
+    vm.hrState = true;
+    vm.mainState = false;
+    vm.hangoutState = true;
+
+    vm.mainSwitch = function() {
+      vm.mainState = false;
+      vm.mksState = true;
+      vm.hrState = true;
+      vm.hangoutState = true;
+    }
+
+    vm.mksSwitch = function() {
+      vm.mainState = true;
+      vm.mksState = false;
+      vm.hrState = true;
+      vm.hangoutState = true;
+    }
+
+    vm.hrSwitch = function() {
+      vm.mainState = true;
+      vm.mksState = true;
+      vm.hrState = false;
+      vm.hangoutState = true;
+    }
+
+    vm.hangoutSwitch = function() {
+      vm.mainState = true;
+      vm.mksState = true;
+      vm.hrState = true;
+      vm.hangoutState = false;
+    }
 
     if(userInfo.isLogged === false){
-      $location.path('/');
+      $location.path('/login');
     }
     // gonna make a global init, followed by 4 other chat room init.
-    vm.init = function() {
+
+    vm.main = function() {
       $http.get('/api/user/getmainchat')
       .then(function(results){
         vm.msgs = results.data.chat
       })
     }
 
-    vm.sendMessage = function() {
-      socket.emit('send-message', vm.messagePrompt + vm.msg.text);
+    vm.mks = function() {
+      $http.get('/api/user/getmks')
+      .then(function(results){
+        vm.mksmsgs = results.data.chat
+      })
+    }
+
+    vm.hr = function() {
+      $http.get('/api/user/gethr')
+      .then(function(results){
+        vm.hrmsgs = results.data.chat
+      })
+    }
+
+    vm.hangout = function() {
+      $http.get('/api/user/gethangout')
+      .then(function(results){
+        vm.hangoutmsgs = results.data.chat
+      })
+    }
+
+    vm.sendMessageMain = function() {
+      socket.emit('send-message-main', vm.messagePrompt + vm.msg.text);
       vm.msg.text = '';
     };
 
-    socket.on('get-message', function(message){
+    socket.on('get-message-main', function(message){
       userInfo.currentMessages = message;
       vm.msgs.push(message);
       $scope.$digest();
-      // $http.post('/api/user/mainchat', userInfo)
-      // .success(function(res){
-      //   console.log("updated!")
-      // }).error(function(err){
-      //   console.log("ERROR", err);
-      // })
+    });
+
+    vm.sendMessageMKS = function() {
+      socket.emit('send-message-mks', vm.messagePrompt + vm.mksmsg.text);
+      vm.mksmsg.text = '';
+    };
+
+    socket.on('get-message-mks', function(message){
+      userInfo.currentMessages = message;
+      vm.mksmsgs.push(message);
+      $scope.$digest();
+    });
+
+    vm.sendMessageHr = function() {
+      socket.emit('send-message-hr', vm.messagePrompt + vm.hrmsg.text);
+      vm.hrmsg.text = '';
+    };
+
+    socket.on('get-message-hr', function(message){
+      userInfo.currentMessages = message;
+      vm.hrmsgs.push(message);
+      $scope.$digest();
+    });
+
+    vm.sendMessageHangout = function() {
+      socket.emit('send-message-hangout', vm.messagePrompt + vm.hangoutmsg.text);
+      vm.hangoutmsg.text = '';
+    };
+
+    socket.on('get-message-hangout', function(message){
+      userInfo.currentMessages = message;
+      vm.hangoutmsgs.push(message);
+      $scope.$digest();
     });
 
   });
--- server.js
+++ server.js
@@ -39,7 +39,43 @@ Chat.findOne({name: "main"}, function(err, user){
   } else {
     console.log("Main Exists!")
   }
-})
+});
+
+Chat.findOne({name: "MKS049"}, function(err, user){
+  console.log("THE USER ", user)
+  if(user === null) {
+    Chat.create({
+      "name": "MKS049",
+      "chat": []
+    })
+  } else {
+    console.log("MKS Exists!")
+  }
+});
+
+Chat.findOne({name: "The Hangout"}, function(err, user){
+  console.log("THE USER ", user)
+  if(user === null) {
+    Chat.create({
+      "name": "The Hangout",
+      "chat": []
+    })
+  } else {
+    console.log("The Hangout Exists!")
+  }
+});
+
+Chat.findOne({name: "HackReactor"}, function(err, user){
+  console.log("THE USER ", user)
+  if(user === null) {
+    Chat.create({
+      "name": "HackReactor",
+      "chat": []
+    })
+  } else {
+    console.log("HackReactor Exists!")
+  }
+});
 
 // Express Routes
 
@@ -50,8 +86,25 @@ app.post('/api/user/signup', function(req, res) {
 })
 
 app.get('/api/user/getmainchat', function(req, res){
-  console.log("Current Chat" + req.body);
   Chat.findOne({name: 'main'}, function(err, messages){
+    res.send(messages)
+  })
+})
+
+app.get('/api/user/getmks', function(req, res){
+  Chat.findOne({name: "MKS049"}, function(err, messages){
+    res.send(messages)
+  })
+})
+
+app.get('/api/user/gethr', function(req, res){
+  Chat.findOne({name: 'HackReactor'}, function(err, messages){
+    res.send(messages)
+  })
+})
+
+app.get('/api/user/gethangout', function(req, res){
+  Chat.findOne({name: 'The Hangout'}, function(err, messages){
     res.send(messages)
   })
 })
@@ -68,19 +121,47 @@ app.post('/api/user/login', function(req, res) {
 // Socket.io listeners and emitters
 
 io.sockets.on('connection', function(socket){
-  socket.removeAllListeners()
-  socket.on('disconnect', function () {
-    setTimeout(function () {
-         //do something
-    }, 10000);
-  });
-  socket.on('send-message', function(data){
+  // socket.removeAllListeners()
+  // socket.on('disconnect', function () {
+  //   setTimeout(function () {
+  //        //do something
+  //   }, 10000);
+  // });
+
+  //socket listeners for main chat room
+
+  socket.on('send-message-main', function(data){
     console.log("SOCKETDATA ",data)
-    io.sockets.emit('get-message', data)
+    io.sockets.emit('get-message-main', data)
     Chat.findOneAndUpdate({name: 'main'}, {$push: { chat: data } }, {upsert:true}, function(err, message){
       console.log("AHHHHH!")
     })
   })
+
+  socket.on('send-message-mks', function(data){
+    console.log("SOCKETDATA ",data)
+    io.sockets.emit('get-message-mks', data)
+    Chat.findOneAndUpdate({name: 'MKS049'}, {$push: { chat: data } }, {upsert:true}, function(err, message){
+      console.log("MKS!")
+    })
+  })
+
+  socket.on('send-message-hr', function(data){
+    console.log("SOCKETDATA ",data)
+    io.sockets.emit('get-message-hr', data)
+    Chat.findOneAndUpdate({name: 'HackReactor'}, {$push: { chat: data } }, {upsert:true}, function(err, message){
+      console.log("HR!")
+    })
+  })
+
+  socket.on('send-message-hangout', function(data){
+    console.log("SOCKETDATA ",data)
+    io.sockets.emit('get-message-hangout', data)
+    Chat.findOneAndUpdate({name: 'The Hangout'}, {$push: { chat: data } }, {upsert:true}, function(err, message){
+      console.log("Hangout!")
+    })
+  })
+
 })
 
 server.listen(port, function(){
